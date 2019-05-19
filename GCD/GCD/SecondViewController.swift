79f51beb7ab95faf3b336ce995c0f2b3fa579a97
Kostya Likhachev <arrow.koss@gmail.com>
2019-05-19 17:09:07 +0300

    - added implementation fetch Image to ImageView from URL - added activityIndicator

--- GCD/GCD/SecondViewController.swift
+++ GCD/GCD/SecondViewController.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndecator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+              return imageView.image
+        }
+        set {
+            activityIndecator.stopAnimating()
+            activityIndecator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
     }
-    */
-
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/New_york_times_square-terabass.jpg/1200px-New_york_times_square-terabass.jpg")
+        activityIndecator.isHidden = false
+        activityIndecator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
+    }
 }
